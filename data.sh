d8f9fc8a005dee10395ff1d3c61516a39f6626f9
pe-hy <petr.hyner10@volny.cz>
2025-08-27 09:46:49 +0200

    karolina

--- data.sh
+++ data.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-#SBATCH --job-name=train_modded
-#SBATCH --output=logs/train/train_%j.out
-#SBATCH --error=logs/train/train_%j.err
+#SBATCH --job-name=data_modded
+#SBATCH --output=logs/data/data_%j.out
+#SBATCH --error=logs/data/data_%j.err
 #SBATCH --time=00:10:00
 #SBATCH --account=OPEN-34-14
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --gpus=8
+#SBATCH --gpus=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=32GB
+#SBATCH --mem=8GB
 #SBATCH --partition=qgpu
 
 module load Anaconda3/2024.02-1
 module load CUDA/12.4.0
 source activate main_env
 
-torchrun --standalone --nproc_per_node=8 train_gpt.py
+python data/cached_fineweb10B.py 8
